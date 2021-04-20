62ab1d80dcda91d097ca1fa2af4416fe152428aa
Kelvi Rafael Dalmazo <kelvi.dalmazo@gmail.com>
2021-04-19 23:48:46 -0300

    feat: initial commit

---
+++ lib/inmana_web/controllers/welcome_controller.ex
@@ -0,0 +1,17 @@
+defmodule InmanaWeb.WelcomeController do
+  use InmanaWeb, :controller
+
+  alias Inmana.Welcomer
+
+  def index(conn, params) do
+    params |> Welcomer.welcome() |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, message}, conn), do: render_response(conn, :ok, message)
+
+  defp handle_response({:error, message}, conn), do: render_response(conn, :bad_request, message)
+
+  defp render_response(conn, status, message) do
+    conn |> put_status(status) |> json(%{message: message})
+  end
+end
